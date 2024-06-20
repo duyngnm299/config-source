322b86927edfc92442c7e36cf8732b1682314293
duyngnm299 <duyngnm.299@gmail.com>
2024-06-20 22:07:56 +0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+// import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/config-source/',
-  server: {
-    port: 3000
-  },
-  css: {
-    devSourcemap: true
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src')
-    }
-  }
+  // server: {
+  //   port: 3000
+  // },
+  // css: {
+  //   devSourcemap: true
+  // },
+  // resolve: {
+  //   alias: {
+  //     '~': path.resolve(__dirname, './src')
+  //   }
+  // }
 })
